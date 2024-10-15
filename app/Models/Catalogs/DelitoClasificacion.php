5b3c8a55cfde44438283062ef071785e1bf15622
Almonte20 <almonte.developer@gmail.com>
2024-10-15 10:15:47 -0600

    Modificación de la tabla de delitos

--- app/Models/Catalogs/DelitoClasificacion.php
+++ app/Models/Catalogs/DelitoClasificacion.php
@@ -8,5 +8,5 @@ use Illuminate\Database\Eloquent\Model;
 class DelitoClasificacion extends Model
 {
     protected $connection = 'sqlsrv';
-    protected $table = 'cat_delito_clasificacion';
+    protected $table = 'cat_delitos';
 }
